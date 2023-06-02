f4a28c2927a33974c0af6422fe330da09bfbe739
angrykoala <angrykoala@outlook.es>
2023-06-02 10:09:55 +0100

    Update config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
-const globalConf = require("../../jest.config.base");
+// const globalConf = require("../../jest.config.base");
 
 module.exports = {
-    ...globalConf,
+    // ...globalConf,
     displayName: "@neo4j/cypher-builder",
     setupFilesAfterEnv: ["jest-extended/all"],
-    roots: ["<rootDir>/packages/cypher-builder/src/", "<rootDir>/packages/cypher-builder/tests/"],
+    roots: ["<rootDir>/src/", "<rootDir>/tests/"],
     coverageDirectory: "<rootDir>/packages/cypher-builder/coverage/",
+    snapshotFormat: {
+        escapeString: true,
+        // printBasicPrototype: true,
+    },
     transform: {
         "^.+\\.ts$": [
             "ts-jest",
             {
-                tsconfig: "<rootDir>/packages/cypher-builder/tsconfig.json",
+                tsconfig: "<rootDir>/tsconfig.json",
             },
         ],
     },
