291ed57407fb4ea34f9788a2ede843afea0324be
angrykoala <angrykoala@outlook.es>
2023-08-08 11:15:58 +0100

    Fix union test variables

--- src/clauses/Union.test.ts
+++ src/clauses/Union.test.ts
@@ -21,47 +21,49 @@ import * as Cypher from "../Cypher";
 
 describe("CypherBuilder UNION", () => {
     test("Union Movies", () => {
+        const returnVar = new Cypher.Variable();
         const n1 = new Cypher.Node({ labels: ["Movie"] });
-        const query1 = new Cypher.Match(n1).return(n1);
+        const query1 = new Cypher.Match(n1).return([n1, returnVar]);
         const n2 = new Cypher.Node({ labels: ["Movie"] });
-        const query2 = new Cypher.Match(n2).return(n2);
+        const query2 = new Cypher.Match(n2).return([n2, returnVar]);
         const n3 = new Cypher.Node({ labels: ["Movie"] });
-        const query3 = new Cypher.Match(n3).return(n3);
+        const query3 = new Cypher.Match(n3).return([n3, returnVar]);
 
         const unionQuery = new Cypher.Union(query1, query2, query3);
         const queryResult = unionQuery.build();
         expect(queryResult.cypher).toMatchInlineSnapshot(`
             "MATCH (this0:Movie)
-            RETURN this0
-            UNION
-            MATCH (this1:Movie)
-            RETURN this1
+            RETURN this0 AS var1
             UNION
             MATCH (this2:Movie)
-            RETURN this2"
+            RETURN this2 AS var1
+            UNION
+            MATCH (this3:Movie)
+            RETURN this3 AS var1"
         `);
         expect(queryResult.params).toMatchInlineSnapshot(`{}`);
     });
 
     test("Union ALL Movies", () => {
+        const returnVar = new Cypher.Variable();
         const n1 = new Cypher.Node({ labels: ["Movie"] });
-        const query1 = new Cypher.Match(n1).return(n1);
+        const query1 = new Cypher.Match(n1).return([n1, returnVar]);
         const n2 = new Cypher.Node({ labels: ["Movie"] });
-        const query2 = new Cypher.Match(n2).return(n2);
+        const query2 = new Cypher.Match(n2).return([n2, returnVar]);
         const n3 = new Cypher.Node({ labels: ["Movie"] });
-        const query3 = new Cypher.Match(n3).return(n3);
+        const query3 = new Cypher.Match(n3).return([n3, returnVar]);
 
         const unionQuery = new Cypher.Union(query1, query2, query3).all();
         const queryResult = unionQuery.build();
         expect(queryResult.cypher).toMatchInlineSnapshot(`
             "MATCH (this0:Movie)
-            RETURN this0
-            UNION ALL
-            MATCH (this1:Movie)
-            RETURN this1
+            RETURN this0 AS var1
             UNION ALL
             MATCH (this2:Movie)
-            RETURN this2"
+            RETURN this2 AS var1
+            UNION ALL
+            MATCH (this3:Movie)
+            RETURN this3 AS var1"
         `);
     });
 });
