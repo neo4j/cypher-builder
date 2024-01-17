ef419ebdff9fb768aea20525464c693fac455aea
MacondoExpress <simone.gammicchia@gmail.com>
2024-01-17 13:32:07 +0000

    fix delete mixin on Cypher.Call

--- src/clauses/Call.test.ts
+++ src/clauses/Call.test.ts
@@ -338,4 +338,34 @@ WITH *"
             }
         `);
     });
+
+    test("CALL with delete", () => {
+        const node = new Cypher.Node({ labels: ["Movie"] });
+        const movie = new Cypher.Variable();
+
+    ;    const matchClause = new Cypher.Match(node)
+            .where(Cypher.eq(new Cypher.Param("aa"), new Cypher.Param("bb")))
+            .return([node.property("title"), movie]);
+
+
+        const clause = new Cypher.Call(matchClause).delete(node);
+        const queryResult = clause.build();
+
+        expect(queryResult.cypher).toMatchInlineSnapshot(`
+"CALL {
+    MATCH (this0:Movie)
+    WHERE $param0 = $param1
+    RETURN this0.title AS var1
+}
+DELETE this0"
+`);
+
+        expect(queryResult.params).toMatchInlineSnapshot(`
+            {
+              "param0": "aa",
+              "param1": "bb",
+            }
+        `);
+    });
+  
 });
