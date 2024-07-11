8f15ed686c4d23351c1ad16fb560d9160e9af7a7
angrykoala <angrykoala@outlook.es>
2024-07-11 16:49:41 +0100

    Add test on quantified patterns with match

--- src/clauses/Match.test.ts
+++ src/clauses/Match.test.ts
@@ -141,6 +141,39 @@ describe("CypherBuilder Match", () => {
         `);
     });
 
+    test("Quantified pattern", () => {
+        const m = new Cypher.Node();
+        const m2 = new Cypher.Node();
+
+        const quantifiedPath = new Cypher.QuantifiedPath(
+            new Cypher.Pattern(m, { labels: ["Movie"], properties: { title: new Cypher.Param("V for Vendetta") } }),
+            new Cypher.Pattern({ labels: ["Movie"] })
+                .related({ type: "ACTED_IN" })
+                .to({ labels: ["Person"] })
+                .quantifier({ min: 1, max: 2 }),
+            new Cypher.Pattern(m2, {
+                labels: ["Movie"],
+                properties: { title: new Cypher.Param("Something's Gotta Give") },
+            })
+        );
+
+        const matchQuery = new Cypher.Match(quantifiedPath).return(m2);
+        const queryResult = matchQuery.build();
+        expect(queryResult.cypher).toMatchInlineSnapshot(`
+"MATCH (this0:Movie { title: $param0 })
+      ((:Movie)-[:ACTED_IN]->(:Person)){1,2}
+      (this1:Movie { title: $param1 })
+RETURN this1"
+`);
+
+        expect(queryResult.params).toMatchInlineSnapshot(`
+{
+  "param0": "V for Vendetta",
+  "param1": "Something's Gotta Give",
+}
+`);
+    });
+
     describe("Assign to path variable", () => {
         const a = new Cypher.Node();
         const b = new Cypher.Node();
