f6532ceeb762678f38a1bd8351a6d703748f0497
angrykoala <angrykoala@outlook.es>
2024-11-19 16:34:52 +0000

    Update eslint to version 9

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import tsdoc from "eslint-plugin-tsdoc";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ["**/dist", "**/docs", "**/coverage", "**/examples", "**/jest.config.js", "eslint.config.mjs"],
+    },
+    ...compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+    ),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            tsdoc,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 5,
+            sourceType: "commonjs",
+
+            parserOptions: {
+                projectService: true,
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/no-unsafe-declaration-merging": "off",
+            "@typescript-eslint/prefer-readonly": "error",
+
+            "@typescript-eslint/consistent-type-imports": [
+                "error",
+                {
+                    prefer: "type-imports",
+                },
+            ],
+
+            "tsdoc/syntax": "warn",
+        },
+    },
+];
